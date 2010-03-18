6f436c843307fbf80ea85bf399a77feca6f41b98
Jon Fuller <fullerjc@gmail.com>
2010-03-18 10:48:10 -0500

    reformat

--- src/Pomodo7o/Program.cs
+++ src/Pomodo7o/Program.cs
@@ -15,21 +15,21 @@ namespace Pomodo7o
             {
                 cfg.For<ITaskbarManager>()
                     .ConditionallyUse(x =>
-                      {
-                          x.If(c => MSTaskbarManager.IsPlatformSupported)
-                           .ThenIt.Is.ConstructedBy(() =>
-                               new TaskbarManager(MSTaskbarManager.Instance));
-                          x.TheDefault.IsThis(new FakeTaskbarManager());
-                      });
+                    {
+                        x.If(c => MSTaskbarManager.IsPlatformSupported)
+                         .ThenIt.Is.ConstructedBy(() =>
+                             new TaskbarManager(MSTaskbarManager.Instance));
+                        x.TheDefault.IsThis(new FakeTaskbarManager());
+                    });
 
                 cfg.For<ViewModel>().Singleton().Use<ViewModel>();
                 cfg.For<Pomodo7oWindow>().Singleton().Use<Pomodo7oWindow>();
 
                 cfg.Scan(scanner =>
-                 {
-                     scanner.AssembliesFromPath("Extensions");
-                     scanner.AddAllTypesOf<IPomodoroPublisher>();
-                 });
+                {
+                    scanner.AssembliesFromPath("Extensions");
+                    scanner.AddAllTypesOf<IPomodoroPublisher>();
+                });
 
                 cfg.For<IPomodoroPublisher>()
                     .Use(x => x.GetInstance<Pomodo7oWindow>());
@@ -41,8 +41,8 @@ namespace Pomodo7o
                     .Ctor<IProgressBar>().Is(x => x.GetInstance<ViewModelProgressBar>());
             });
 
-            var app = new Application {MainWindow = ObjectFactory.GetInstance<Pomodo7oWindow>()};
-            using(ObjectFactory.GetInstance<AppController>())
+            var app = new Application { MainWindow = ObjectFactory.GetInstance<Pomodo7oWindow>() };
+            using (ObjectFactory.GetInstance<AppController>())
             {
                 app.Run(app.MainWindow);
             }
