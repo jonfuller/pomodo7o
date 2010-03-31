9003427144250caf6a376195236614e4f9d45f93
Jon Fuller <fullerjc@gmail.com>
2010-03-31 12:03:26 -0400

    updated assembly info, after generation

--- src/Pomodo7o/Properties/AssemblyInfo.cs
+++ src/Pomodo7o/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 [assembly: AssemblyTitle("Pomodo7o")]
 [assembly: AssemblyDescription("A Pomodoro timer that highlights Windows 7 taskbar features.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jon Fuller")]
 [assembly: AssemblyProduct("Pomodo7o")]
-[assembly: AssemblyCopyright("Copyright © Jon Fuller 2010")]
+[assembly: AssemblyCopyright("Copyright Jon Fuller 2010")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
 
-[assembly: ComVisible(false)]
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,
-    ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
